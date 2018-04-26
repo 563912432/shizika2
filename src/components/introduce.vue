14759a14d5369143581762998a698b47648def83
陈帅 <1301491593@qq.com>
2018-04-26 17:43:49 +0800

    'video'

--- src/components/introduce.vue
+++ src/components/introduce.vue
@@ -17,8 +17,9 @@
     mounted () {
       let vid = 'dbadf91f04bed81ee5431384c86abdb0_d'
       this.get(this.host + 'Api/Video/getPolySign/code/' + vid, res => {
-        if (res.status) {
-          let polySign = JSON.parse(res.info)
+        let response = JSON.parse(res)
+        if (response.status) {
+          let polySign = JSON.parse(response.info)
           let videoHeight = 200
           let height = window.screen.height
           let viewHeight = window.innerHeight
